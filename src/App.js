fdca389106b04971d18ddbc91d88fd6140255601
Tiago Moreira <tsm_projetos@hotmail.com>
2020-04-10 02:08:38 -0300

    Configuração do ambiente de desenvolvimento: Git/Github, React, dependências iniciais (bootstrap, jquery, popper.js, fontawesome).

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (     
+      <div>
+        <p>Hello wondeful world!</p>
+        <i class="fa fa-heart"></i>
+        <i class="fab fa-cc-amex"></i>
+        <i class="fa fa-shopping-bag"></i>
+      </div>       
+      
+    )
+  }
+}
---
+++ src/include/bootstrap.js
@@ -0,0 +1,6 @@
+import './jquery'
+import './popper.js'
+
+import 'bootstrap'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
