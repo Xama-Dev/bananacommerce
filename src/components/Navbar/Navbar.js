046961db01f21db946610ac4812197206aa97601
Tiago Moreira <tsm_projetos@hotmail.com>
2020-04-15 03:53:46 -0300

    implementado menu dropdown dos departamentos.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -22,85 +22,85 @@ class Navbar extends Component {
                     </div>
                     <div className="container-menu-departments-options">
                         <ul>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                     <FontAwesomeIcon icon={['far','star']}/>
                                     <span> Hot Promotions</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','tv']}/>
                                 <span> Consumer Electrics</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','tshirt']}/>
                                 <span>  Clothing &amp; Apparel</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','couch']}/>
                                 <span> Home, Garden &amp; Kitchen</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','heartbeat']}/>
                                 <span> Health &amp; Beauty</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['far','gem']}/>
                                 <span> Jewelry &amp; Watches</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','laptop']}/>
                                 <span> Computers &amp; Technologies</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','baby-carriage']}/>
                                 <span> Babies &amp; Moms</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','futbol']}/>
                                 <span> Sport &amp; Outdoor</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','mobile-alt']}/>
                                 <span> Phones &amp; Accessories</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','book']}/>
                                 <span> Books &amp; Office</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','car']}/>
                                 <span> Cars &amp; Motocycles</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','tools']}/>
                                 <span> Home Improments</span>
                                 </li>
                             </a>
-                            <a href="/">
+                            <a href="/ ">
                                 <li>
                                 <FontAwesomeIcon icon={['fas','tag']}/>
                                 <span> Vouchers &amp; Services</span>
